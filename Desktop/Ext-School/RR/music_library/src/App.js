a3a12831ba7bad26b3c32a5d0c239c1d18076cfd
Jose Torres <jrtp22@gmail.com>
2023-09-05 22:21:06 -0700

    added routes

--- Desktop/Ext-School/RR/music_library/src/App.js
+++ Desktop/Ext-School/RR/music_library/src/App.js
@@ -1,7 +1,10 @@
 
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
   let [search, setSearch] = useState('')
@@ -33,11 +36,21 @@ function App() {
   }
   return (
     <div>
-      <SearchBar handleSearch={handleSearch} />
-      {message}
-      <Gallery data={data} />
+    {message}
+        <Router>
+            <Routes>
+                <Route path="/" element={
+                    <Fragment>
+                        <SearchBar handleSearch = {handleSearch}/>
+                        <Gallery data={data} />
+                    </Fragment>
+                } />
+                <Route path="/album/:id" element={<AlbumView />} />
+                <Route path="/artist/:id" element={<ArtistView />} />
+            </Routes>
+        </Router>
     </div>
-  )
-}
+)
+
 
 export default App
